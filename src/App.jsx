d626eafcc0fa844c500d315daac4492878984354
Gonzalo Ibarrola <gonzaloibarrola95@gmail.com>
2023-05-05 21:12:55 -0300

    IsLoading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Result from './assets/components/Result'
 import Summary from './assets/components/Summary'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
+  
 
   return (
     <main>
+      { isLoading ? <p>Cargando</p> : 
       <article className='bg-White shadow-1 h-screen md:h-[512px] w-full md:max-w-[736px] md:inline-flex md:rounded-[32px]'>
-        <Result />
-        <Summary />
+      <Result />
+      <Summary />
       </article>
+      }
     </main>
   )
 }
